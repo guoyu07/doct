a882d81ed06f08c6039f210e64307e486b8535d2
lengjingxu <lengjingxu@gmail.com>
2015-09-26 01:12:00 +0200

    搜索结果

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -68,12 +69,16 @@
 						</li>
 					</ul>
 					<form class="navbar-form navbar-left" role="search">
+						<form action="main.php" method="get">
 						<div class="form-group">
-							<input type="text" class="form-control">
+							<input type="text" class="form-control" name="search"> 
+							
 						</div> 
 						<button type="submit" class="btn btn-default">
 							Submit
 						</button>
+						</form>
+
 					</form>
 					<ul class="nav navbar-nav navbar-right">
 						<li>
@@ -109,55 +114,9 @@
 		</div>
 		<div class="col-md-10">
 			<div class="row">
-			获取从main获得的结果插入到 grid中
-				<div class="col-md-4">
-					<div class="thumbnail">
-						<img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg">
-						<div class="caption">
-							<h3>
-								Thumbnail label
-							</h3>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							</p>
-							<p>
-								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
-							</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="thumbnail">
-						<img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg">
-						<div class="caption">
-							<h3>
-								Thumbnail label
-							</h3>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							</p>
-							<p>
-								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
-							</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="thumbnail">
-						<img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg">
-						<div class="caption">
-							<h3>
-								Thumbnail label
-							</h3>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							</p>
-							<p>
-								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
-							</p>
-						</div>
-					</div>
-				</div>
+			
+			<?php include 'main.php' ?>
+			
 			</div>
 			<ul class="pagination">
 				<li>
--- main.php
+++ main.php
@@ -1,4 +1,49 @@
 
 <?php
+ 	include ( 'config.php' );
 
+	$servername = "localhost";
+$username = "doc";
+$password = "";
+$dbname = "doc";
+	
+$keyWord= $_GET["search"]= 'Paris'; 
+$page=  $_GET["page"]= '1';
+  $con = mysql_connect("$servername","$username","$password");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("$dbname", $con);
+
+
+$result = mysql_query("SELECT * FROM `monamphi` WHERE `ecole` LIKE '%$keyWord%' OR `matirie` LIKE '%$keyWord%' OR `docname` LIKE '%$keyWord%' OR `prof` LIKE '%$keyWord%' ORDER BY 1 DESC LIMIT 10");
+
+
+
+while($row = mysql_fetch_array($result))
+  {
+
+  
+  
+  echo ' <div class="col-md-4">
+					<div class="thumbnail">
+						<div class="caption">
+							<h3>
+							'; echo $row['docname'] . '
+							</h3>
+							<p>
+							';
+							echo $row['ecole'] .$row['matirie'].$row['prof']."<br>".$row['niveau'].$row['annee'].$row['type'].'</p><p><a class="btn btn-primary" href="';
+							echo $row['id'];
+	echo '">Action</a> <a class="btn" href="#">Action</a>
+							</p>
+						</div>
+					</div>
+				</div>';
+  }
+
+mysql_close($con);
+?>
 //搜索结果 ，返回json,在首页ajax获取信息
